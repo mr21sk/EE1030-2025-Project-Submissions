6dfbe09ab36ecafbedc4c6cc5f20206da5c5584f
hemahavil <hemahavilsambangi@gmail.com>
2025-11-01 01:16:36 +0530

    ee25btech11050 ee25btech1139 - EE1030 Hardware Project Submission

---
+++ ee25btech11050_ee25btech11039/Hardware-Assignment/codes/arduino/major.cpp
@@ -0,0 +1,35 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.clear();
+  Serial.begin(9600);
+  lcd.print("Digital");
+  lcd.setCursor(0, 1);
+  lcd.print("Thermometer");
+  delay(1500);
+  lcd.clear();
+  lcd.print("Temperature");
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  double voltage = (5.0 * sensorValue / 1023.0);
+  double temp = -891.347626 * pow(voltage, 3) 
+                + 2834.068197 * pow(voltage, 2) 
+                - 2209.623323 * voltage 
+                + 816.9973454;
+
+  Serial.println(voltage, 4);
+  delay(500);
+  
+  lcd.setCursor(0, 1);
+  lcd.print(temp, 2);
+  lcd.write(0xDF); // Degree symbol
+  lcd.print("C");
+  delay(1000);
+}
+
