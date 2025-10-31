66cc6ec345f5b5132e8e0a914f5d56e4ca36ecaa
vishwambhar-ee25btech11025 <vishwambhar.450@gmail.com>
2025-11-01 00:36:18 +0530

    send pullrequest

--- Hardware-Assignment/codes/linear_regression/linear_regression_inverse/linear_regression_inverse.py
+++ Hardware-Assignment/codes/linear_regression/linear_regression_inverse/linear_regression_inverse.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 
-x_train, y_train = np.loadtxt("../../tables/training_data.txt", unpack=True)
+y_train, x_train = np.loadtxt('../../../tables/training_data.txt', unpack=True)
 
-x_val, y_val = np.loadtxt("../../tables/validation_data.txt", unpack=True)
+y_val, x_val = np.loadtxt('../../../tables/validation_data.txt', unpack=True)
 
 A = np.vstack([np.ones_like(x_train), x_train, x_train**2, x_train**3]).T
 b = y_train.reshape(-1, 1)
@@ -22,27 +22,26 @@ print("Estimated cubic coefficients (from training data):")
 for i, c in enumerate(coeffs):
     print(f"  c{i} = {c:.6f}")
 
-xx = np.linspace(23, 96, 100)
+xx = np.linspace(2.15, 2.42, 200)
 yy_fit = coeffs[0] + coeffs[1]*xx + coeffs[2]*xx**2 + coeffs[3]*xx**3
 
-plt.figure(1, figsize=(8, 6))
+plt.figure(1, figsize=(8, 6)) 
 plt.scatter(x_train, y_train, color='blue', label="Training Data Points")
 plt.plot(xx, yy_fit, color='red', label="Inverse Least squares cubic fit")
-plt.ylabel("Voltage(V)")
-plt.xlabel("Temperature(T)")
+plt.ylabel("Temperature(C)")
+plt.xlabel("Voltage(V)")
 plt.title("Inverse Least Squares Fit on Training Data")
 plt.legend()
 plt.grid(True)
-plt.savefig("../../figs/voltage_temp/train.png")
-plt.show()
+plt.savefig("../../../figs/plot_inverse/training.png")
 
-plt.figure(2, figsize=(8, 6))
+plt.figure(2, figsize=(8, 6)) 
 plt.scatter(x_val, y_val, color='green', label="Validation Data Points")
 plt.plot(xx, yy_fit, color='red', label="Inverse Least square prediction")
-plt.ylabel("Voltage(V)")
-plt.xlabel("Temperature(T)")
+plt.ylabel("Temperature(C)")
+plt.xlabel("Voltage(V)")
 plt.title("Validation Data")
 plt.legend()
 plt.grid(True)
-plt.savefig("../../figs/voltage_temp/valid.png")
+plt.savefig("../../../figs/plot_inverse/validation.png")
 plt.show()
