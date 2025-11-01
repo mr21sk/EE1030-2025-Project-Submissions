029c85b913c65827ac0af19a5b51c066047cf179
arshdhoke109-gif <arshdhoke109@gmail.com>
2025-11-01 07:09:20 +0530

    ee25btech11010 ee25btech11009 - EE1030 Hardware Project Submission

--- ee25btech11010_ee25btech11009/Hardware-Assignment/codes/linear_regression/error.py
+++ ee25btech11010_ee25btech11009/Hardware-Assignment/codes/linear_regression/error.py
@@ -1,19 +1,34 @@
-# Sample observed and calculated temperature values
-observed = [66.3, 50.1, 74.9, 86.0, 37.6]
-calculated = [67.4, 49.7, 74.9, 84.5, 35.1]
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Lists to store errors
-absolute_error = []
-percentage_error = []
+A = np.loadtxt('../linear_regression/training_data.txt')
+X = np.hstack((np.ones((A.shape[0],1)), A[:,[0]], A[:,[0]]**2))
+T = A[:,[0]]
+C = A[:,[1]]
 
-for o, c in zip(observed, calculated):
-    abs_err = abs(o - c)
-    percent_err = (abs_err / o) * 100
+n_lsq = np.linalg.lstsq(X, C, rcond=None)[0].reshape(-1, 1)
 
-    absolute_error.append(abs_err)
-    percentage_error.append(percent_err)
+C_pred = X @ n_lsq
 
-# Print results
-print("Observed | Calculated | Absolute Error | Percentage Error (%)")
-for o, c, ae, pe in zip(observed, calculated, absolute_error, percentage_error):
-    print(f"{o:.2f}      {c:.3f}        {ae:.3f}           {pe:.2f}%")
+error_train = C - C_pred
+mse_train = np.mean(error_train**2)
+print(f"Training Quadratic Loss (MSE): {mse_train:.8e}")
+
+plt.figure(figsize=(7,5))
+plt.scatter(T, error_train, color='red', label='Error points')
+plt.plot(T, error_train, color='blue', linewidth=1, label='Error curve')
+plt.axhline(0, color='black', linestyle='--')
+plt.xlabel('Temperature (°C)')
+plt.ylabel('Error (V_actual - V_pred)')
+plt.title(f'Training Data Error (MSE = {mse_train:.6f})')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.savefig('../../figs/train_error.png', dpi=300)
+plt.close()
+
+
+
+
+
+
